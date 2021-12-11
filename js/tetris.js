c0e1e4f506b7533bc759cc86f19e5c8b5e50f192
ourainbows <casasjuanfelipe@gmail.com>
2021-12-10 20:16:36 -0500

    Agregado evento de touch

--- js/tetris.js
+++ js/tetris.js
@@ -3,14 +3,15 @@ let canvas = document.getElementById("tetris")
 let ctx = canvas.getContext("2d")
 
 // paint canvas
-ctx.fillStyle = "red"
+ctx.fillStyle = "black"
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 
 
-// Create 7 pieces of tetris
+/*This function create the 7 pieces of tetris
+    PieceName -> A vowel to select one figure
+    return -> array than contains the piece */ 
 function createPiece(pieceName) {
-    
     switch (pieceName) {
         case "O":
             return [
@@ -42,14 +43,14 @@ function createPiece(pieceName) {
                 [1, 1, 0],
                 [0, 1, 1]
             ]
-        case "J":
+        case "L":
             return [
                 [0,1,0,0],
                 [0,1,0,0],
                 [0,1,0,0],
                 [0,1,1,0]
             ]
-        case "L":
+        case "J":
             return [
                 [0,0,1,0],
                 [0,0,1,0],
@@ -58,3 +59,23 @@ function createPiece(pieceName) {
             ]
     }
 }
+document.addEventListener("keydown", function (key) {
+    console.log(key.key)
+    if (key.key == "ArrowUp") {
+        //Piece.up()
+    }
+    if (key.key == "ArrowDown") {
+        //Piece.down()
+    }
+    if (key.key == "ArrowLeft") {
+        //Piece.left()
+    }
+    if (key.key == "ArrowRight") {
+        //Piece.right()
+    }
+})
+
+// swiped-left
+canvas.addEventListener("touchstart", function () {
+    console.log("Hello World")
+})
