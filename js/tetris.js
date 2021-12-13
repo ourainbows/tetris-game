120bdbe5d09df92a228f9fe039d60d5b721c8123
ourainbows <casasjuanfelipe@gmail.com>
2021-12-13 01:36:19 -0500

    La pieza se mueve cada segundo

--- js/tetris.js
+++ js/tetris.js
@@ -2,34 +2,75 @@
 let canvas = document.getElementById("tetris")
 let ctx = canvas.getContext("2d")
 
-// paint canvas
-ctx.fillStyle = "black"
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+//Change size to elemtents than will display
+ctx.scale(20, 20)
 
 
+
+let piece = {
+    posX: 0,
+    posY: 0,
+}
+
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+]
+
+//Offset parameters help to change the position of x and y where piece appears
+function drawPiece(offsetX,offetY) {
+    for (y = 0; y < matrix.length; y++) {
+        for (x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] != 0) {
+                ctx.fillStyle = "red"
+                ctx.fillRect(x + offsetX, y + offetY, 1, 1)
+            }
+        }
+    }
+}
+
+// draw
+function draw() {
+    // paint canvas
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    // draw a figure 
+    drawPiece(piece.posX, piece.posY)
+    piece.posY++
+}
+
+function update() {
+    //draw()
+    //requestAnimationFrame(update)
+    setInterval(function () {
+        draw()
+    }, 1000 / 1)
+}
+update()
 
 /*This function create the 7 pieces of tetris
     PieceName -> A vowel to select one figure
-    return -> array than contains the piece */ 
+    return -> array than contains the piece */
 function createPiece(pieceName) {
     switch (pieceName) {
         case "O":
             return [
-                [1,1],
-                [1,1]
+                [1, 1],
+                [1, 1]
             ]
         case "I":
             return [
-                [0,1,0,0],
-                [0,1,0,0],
-                [0,1,0,0],
-                [0,1,0,0]
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0]
             ]
         case "T":
             return [
-                [0,0,0],
-                [1,1,1],
-                [0,1,0]
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0]
             ]
         case "S":
             return [
@@ -45,37 +86,46 @@ function createPiece(pieceName) {
             ]
         case "L":
             return [
-                [0,1,0,0],
-                [0,1,0,0],
-                [0,1,0,0],
-                [0,1,1,0]
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0]
             ]
         case "J":
             return [
-                [0,0,1,0],
-                [0,0,1,0],
-                [0,0,1,0],
-                [0,1,1,0]
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 1, 1, 0]
             ]
     }
 }
+
+
 document.addEventListener("keydown", function (key) {
     console.log(key.key)
     if (key.key == "ArrowUp") {
-        //Piece.up()
+        //Piece.rotate
     }
     if (key.key == "ArrowDown") {
-        //Piece.down()
+        //Piece.down
+        piece.posY++
     }
     if (key.key == "ArrowLeft") {
         //Piece.left()
+        piece.posX--
     }
     if (key.key == "ArrowRight") {
         //Piece.right()
+        piece.posX++
     }
 })
 
-// swiped-left
+
+
+
+
+// touch
 canvas.addEventListener("touchstart", function () {
-    console.log("Hello World")
+    //rotate piece
 })
